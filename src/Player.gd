7537d83c9aa82aaba094f78f350819fa267ed7f0
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-04-24 13:38:16 +0200

    start writing player movement

--- src/Player.gd
+++ src/Player.gd
@@ -1,11 +1,23 @@
 extends KinematicBody2D
 
 
-var speed = 0.1
+var max_x_speed = 0.1
+var y_speed = 0.1
 var velocity
 
 func _ready():
-	velocity = Vector2(0, speed)
+	velocity = Vector2(0, y_speed)
+
 
 func _process(delta):
 	move_and_collide(velocity)
+	handle_input(delta)
+
+
+func handle_input(delta):
+	if Input.is_action_pressed("left"):
+		if velocity.x > -max_x_speed:
+			velocity.x -= 0.5 * delta
+	if Input.is_action_pressed("right"):
+		if velocity.x < max_x_speed:
+			velocity.x += 0.5 * delta
