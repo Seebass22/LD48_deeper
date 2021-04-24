061852fa50ee3368d3e51575fad0390376ca343b
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-04-24 18:11:17 +0200

    fix crate animation restarting after multiple hits

--- src/Crate.gd
+++ src/Crate.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
+var hit = false
+
 func explode():
-	$AnimatedSprite.frame = 1
-	$AnimatedSprite.play()
+	if not hit:
+		hit = true
+		$AnimatedSprite.frame = 1
+		$AnimatedSprite.play()
 
 
 func _on_AnimatedSprite_animation_finished():
