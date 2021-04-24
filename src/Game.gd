6eeea7465964284783f3745106d0b1dbb9684b7a
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-04-24 14:53:51 +0200

    update tile generation

--- src/Game.gd
+++ src/Game.gd
@@ -1,15 +1,46 @@
 extends Node2D
 
-var wall_width = 2
+var wall_width = 3
 var tunnel_width = 8
+var segment_size = 16
+
+var current_x_offset = 0
+var current_y = 0
+var current_direction = 0
+
 
 func _ready():
-	for y in range(16):
+	$Player.connect("should_generate_segment", self, "add_segment")
+	add_segment()
+	add_segment()
+
+
+func generate_segment(x_pos, y_pos):
+	for y in range(segment_size):
+		y += y_pos
 		for x in range(wall_width):
+			x += x_pos
 			$Walls.set_cell(x, y, 0)
 
-	for y in range(16):
+	for y in range(segment_size):
+		y += y_pos
 		for x in range(wall_width):
+			x += x_pos
 			$Walls.set_cell(x + wall_width + tunnel_width, y, 0)
 
-	$Walls.update_bitmask_region(Vector2(0.0, 0.0), Vector2(16, 16))
+	var start_pos = Vector2(x_pos, y_pos)
+	var end_pos = Vector2(x_pos + wall_width*2 + tunnel_width, y_pos + segment_size)
+	$Walls.update_bitmask_region(start_pos, end_pos)
+
+	current_y += segment_size
+
+
+func add_segment():
+	var change_direction_chance = 20
+
+	randomize()
+	if randi() % 100 < change_direction_chance:
+		print('change direction')
+		current_x_offset += pow(-1, randi() % 2 + 1)
+
+	generate_segment(current_x_offset, current_y)
--- src/Player.gd
+++ src/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
-
 var max_x_speed = 0.1
 var y_speed = 0.1
 var velocity
+
+var next_segment_generation_y = 1024
+
+signal should_generate_segment
+
 
 func _ready():
 	velocity = Vector2(0, y_speed)
@@ -12,6 +16,11 @@ func _ready():
 func _process(delta):
 	move_and_collide(velocity)
 	handle_input(delta)
+
+	if position.y >= next_segment_generation_y:
+		print(position.y)
+		next_segment_generation_y += 1024
+		emit_signal("should_generate_segment")
 
 
 func handle_input(delta):
