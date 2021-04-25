ce4cdbf178c8c8b2d9fd1000a8cbcfb812978927
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-04-25 13:59:29 +0200

    add explosion sound

--- src/Crate.gd
+++ src/Crate.gd
@@ -2,6 +2,11 @@ extends StaticBody2D
 
 var hit = false
 
+func _ready():
+	Signals.connect("slow_motion_start", self, "low_pitch_boom")
+	Signals.connect("slow_motion_end", self, "high_pitch_boom")
+
+
 func explode():
 	if not hit:
 		hit = true
@@ -9,8 +14,20 @@ func explode():
 		$CollisionShape2D.set_deferred('disabled', true)
 		$AnimatedSprite.frame = 1
 		$AnimatedSprite.play()
+		$ExplosionSound.play()
 
 
 func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.hide()
+
+
+func _on_ExplosionSound_finished():
 	queue_free()
 
+
+func low_pitch_boom():
+	$ExplosionSound.pitch_scale = 0.4
+
+
+func high_pitch_boom():
+	$ExplosionSound.pitch_scale = 1
