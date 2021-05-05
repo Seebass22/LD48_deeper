5ac6c27122640f23579d39610fb975ec178ca465
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-05-05 17:10:23 +0200

    fix errors by using timer node in menu scenes
    errors caused by using scenetree timer, then switching scenes before
    timeout.

--- src/GameOver.gd
+++ src/GameOver.gd
@@ -4,12 +4,14 @@ export (NodePath) var _score_path
 onready var score_text = get_node(_score_path)
 
 func _ready():
+	var timer = get_node("TileMap/Timer")
 	score_text.set_text("%d" % [Global.score])
 	$PlayAgain.visible = false
 	$MainMenu.visible = false
 	for y in range(2):
 		for x in range(4):
-			yield(get_tree().create_timer(0.05), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+5, y+2, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 
@@ -17,7 +19,8 @@ func _ready():
 
 	for y in range(2):
 		for x in range(4):
-			yield(get_tree().create_timer(0.05), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+6, y+5, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 
--- src/MainMenu.gd
+++ src/MainMenu.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var tile_spawn_delay = 0.03
-
 func _ready():
+	var timer = get_node("TileMap/Timer")
 	$Title.visible = false
 	$Play.visible = false
 	$Quit.visible = false
@@ -11,27 +10,31 @@ func _ready():
 
 	for y in range(6):
 		for x in range(3):
-			yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+1, y, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 
 	for y in range(3):
 		for x in range(3):
-			yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+2, y+6, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 
 	# title BG
 	for x in range(6, 0, -1):
 		for y in range(2):
-			yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+5, y, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 	$Title.visible = true
 
 	# play button BG
 	for x in range(3):
-		yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+		timer.start()
+		yield(timer, "timeout")
 		$TileMap.set_cell(x+5, 3, 0)
 		$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 	$Play.visible = true
@@ -39,7 +42,8 @@ func _ready():
 	# quit button BG
 	if not OS.get_name() == "HTML5":
 		for x in range(3):
-			yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+6, 5, 0)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 		$Quit.visible = true
@@ -47,7 +51,8 @@ func _ready():
 	# panel BG
 	for x in range(5):
 		for y in range(6):
-			yield(get_tree().create_timer(tile_spawn_delay), "timeout")
+			timer.start()
+			yield(timer, "timeout")
 			$TileMap.set_cell(x+10, y+3, 1)
 			$TileMap.update_bitmask_region(Vector2(0,0), Vector2(16,16))
 	$PanelText.visible = true
