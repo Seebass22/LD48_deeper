f6f8cdd84420f8a9fa585464a4e92f64981bafd9
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-01-14 21:08:00 +0100

    use new tileset

--- src/Game.gd
+++ src/Game.gd
@@ -18,31 +18,91 @@ var segment_size = 16
 var current_x_offset = 0
 var current_y = 0
 var current_direction = 0
-
+var tile_names = {
+	"left": ["left1", "left2", "left3"],
+	"right": ["right1", "right2", "right3", "right4"],
+	"mid": ["mid1", "mid2", "mid3", "mid4", "mid5"],
+	"small_corner": ["small_corner1", "small_corner2", "small_corner3", "small_corner4"]
+}
+var tile_ids = {}
+# 1 = right, -1 = left
+var segment_direction = -1
+var is_new_direction = false
 
 func _ready():
+	get_tile_ids()
 	randomize()
-	$Player.connect("should_generate_segment", self, "add_segment")
-	$Player.connect("should_generate_segment", self, "increase_score_distance")
-	Signals.connect("crate_destroyed", self, "increase_score_crate")
-	Signals.connect("target_destroyed", self, "increase_score_target")
-	Signals.connect("target_destroyed", self, "increment_combo")
-	Signals.connect("reset_combo", self, "reset_combo")
-	Signals.connect("game_over", self, "game_over")
+	var _i = $Player.connect("should_generate_segment", self, "add_segment")
+	_i = $Player.connect("should_generate_segment", self, "increase_score_distance")
+	_i = Signals.connect("crate_destroyed", self, "increase_score_crate")
+	_i = Signals.connect("target_destroyed", self, "increase_score_target")
+	_i = Signals.connect("target_destroyed", self, "increment_combo")
+	_i = Signals.connect("reset_combo", self, "reset_combo")
+	_i = Signals.connect("game_over", self, "game_over")
 	add_segment()
 	add_segment()
 	Global.score = 0
 	update_score_ui()
 	reset_combo()
+	print(tile_names)
+	print(tile_ids)
 
+func get_tile_ids():
+	for category in tile_names.keys():
+		tile_ids[category] = []
+		for name in tile_names[category]:
+			tile_ids[category].append($Walls.tile_set.find_tile_by_name(name))
+	
 
+func _set_cell(x, y, tile_id):
+	$Walls.set_cell(x, y, tile_id)
+	$Walls.set_cell(x + wall_width + tunnel_width, y, tile_id)
+	
+func generate_direction_change_segment(x_pos, y_pos):
+	var top_tiles
+	var bot_tiles
+	var offset
+	if segment_direction == 1:
+		top_tiles = ["corner_bot_left", "mid2", "right_bot_side", "small_corner3"]
+		bot_tiles = ["small_corner1", "curve1", "curve2", "corner_top_right"]
+		offset = 1
+	else:
+		top_tiles = ["small_corner2", "curve4", "curve3", "left_bot_side"]
+		bot_tiles = ["corner_top_left", "mid1", "right_top_side", "small_corner4"]
+		offset = 0
+
+	var y = y_pos
+	for tile_row in [top_tiles, bot_tiles]:
+		var x = x_pos - offset
+		for wall_type in tile_row:
+			var tile_id = $Walls.tile_set.find_tile_by_name(wall_type)
+			_set_cell(x, y, tile_id)
+			x +=1
+		y += 1
+	
 func generate_segment(x_pos, y_pos):
-	for y in range(segment_size):
-		y += y_pos
-		for x in range(wall_width):
-			x += x_pos
-			$Walls.set_cell(x, y, 0)
-			$Walls.set_cell(x + wall_width + tunnel_width, y, 0)
+	var segment_height = segment_size
+	var new_segment_y_offset = 0
+	if is_new_direction:
+		is_new_direction = false
+		generate_direction_change_segment(x_pos, y_pos)
+		segment_height -= 2
+		new_segment_y_offset = 2
+		
+	for y in range(segment_height):
+		y += y_pos + new_segment_y_offset
+
+		var x = x_pos
+		for wall_type in ["left", "mid", "right"]:
+			var tiles = tile_ids[wall_type]
+			var tile_id
+			if randi() % 4 == 0:
+				tile_id = tiles[randi() % tiles.size()]
+			else:
+				tile_id = tiles[0]
+			$Walls.set_cell(x, y, tile_id)
+			$Walls.set_cell(x + wall_width + tunnel_width, y, tile_id)
+			x +=1
 
 	var start_pos = Vector2(x_pos, y_pos)
 	var end_pos = Vector2(x_pos + wall_width*2 + tunnel_width, y_pos + segment_size)
@@ -56,7 +116,10 @@ func add_segment():
 
 	var random = randi() % 100
 	if random < change_direction_chance:
-		current_x_offset += pow(-1, randi() % 2 + 1)
+		is_new_direction = 1
+		segment_direction *= -1
+		current_x_offset += segment_direction
+
 		if randi() % 2 == 0:
 			add_crate_segment()
 		else:
@@ -71,7 +134,7 @@ func add_segment():
 
 func spawn_obstacles():
 		var spawn_y = current_y + 4 + randi() % 4
-		for i in range((randi() % 3 ) + 1):
+		for _i in range((randi() % 3 ) + 1):
 			spawn_y += 1
 			spawn_obstacle(spawn_y + 1 + randi() % 5)
 
